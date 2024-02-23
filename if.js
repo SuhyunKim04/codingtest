5e6807c966ff9364caf0278d58b61241e153526d
Suhyun Kim <kim00431@algonquinlive.com>
2024-02-23 17:00:44 -0500

    updates

--- if.js
+++ if.js
@@ -74,3 +74,54 @@ const solution = (str1, str2) => str2.includes(str1) ? 1 : 0;
 //     return +str2.includes(str1);
 //  }
 // **********************************
+
+
+
+function solution(num_list) {
+   let arr = num_list[num_list.length-1]
+   let arr2 = num_list[num_list.length-2]
+   if(arr > arr2) {
+    num_list.push(arr - arr2)
+   }else {
+    num_list.push(arr*2)
+   }
+    return num_list;
+}
+
+
+// function solution(num_list) {
+//     const [a, b] = [...num_list].reverse();
+//     return [...num_list, a > b ? (a-b):a*2];
+// }
+
+
+
+
+
+function solution(n, control) {
+    for(i=0; i<control.length; i++){
+        const arr = control[i]
+        if(arr === "w"){
+            n += 1
+        }if(arr === "s"){
+            n -=1
+        }if(arr === "d"){
+            n += 10
+        }if(arr === "a"){
+            n -= 10
+        }
+    }
+    return n;
+}
+
+
+// const operations = {
+//     w: (n) => n + 1,
+//     s: (n) => n - 1,
+//     d: (n) => n + 10,
+//     a: (n) => n - 10,
+//   };
+  
+//   function solution(n, control) {
+//     return [...control].reduce((prev, op) => operations[op](prev), n);
+//   }
