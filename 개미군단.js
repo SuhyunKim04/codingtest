88be89dca2c9b9c3ad7c481f4fdea1cd85d8460c
Suhyun Kim <kim00431@algonquinlive.com>
2024-04-11 19:57:14 -0400

    updates

--- 개미군단.js
+++ 개미군단.js
@@ -2,6 +2,13 @@
 //이해못함
 
 function solution(hp) {
-    var answer = 0;
-    return answer;
+    var a = Math.floor(hp / 5)
+    var b = Math.floor((hp % 5) / 3)
+   
+
+    var c = hp - (a*5) - (b*3)
+    
+   return a+b+c
 }
+
+console.log(solution(24))
---
+++ 직각삼각형출력하기.js
@@ -0,0 +1,6 @@
+let str = ''
+
+for(let i = 0; i < 3; i++){
+    str += '*'
+    console.log(str)
+}
